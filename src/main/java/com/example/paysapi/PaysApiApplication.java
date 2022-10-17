b2f437d3ac2b2e5efc698a135cf8fdd7eb4271e9
penoitlaplante <benoitlaplante3@gmail.com>
2022-10-16 23:07:09 -0400

    Initial commit

---
+++ src/main/java/com/example/paysapi/PaysApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.paysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaysApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paysapi/PaysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
